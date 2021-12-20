365db7aaaf8c925fee0ddac16d751fdee40e8634
Alexander <aleksandr.nagornov@galaktika-tmn.ru>
2021-12-20 10:22:20 +0500

    houses: update and create endpoint

--- house/controllers/house_controller.py
+++ house/controllers/house_controller.py
@@ -1,9 +1,9 @@
 import json
 
-from app import app
+from app import app, db
 from models.house_model import House
 from services.house_service import hello, create_response
-from flask import jsonify, request, Response
+from flask import request, Response, jsonify
 
 
 @app.route('/')
@@ -15,14 +15,35 @@ def get_houses() -> Response:
 
 @app.route('/house/<house_id>', methods=['GET', 'PUT'])
 def create_update_house(house_id: int):
+    house = House.query.get(house_id)
     if request.method == 'GET':
-        house = House.query.get(house_id)
         if house:
             return jsonify(house.as_dict())
         else:
-            return jsonify(create_response(404, 'not found', 'house not found'))
+            return create_response(404, 'not found', 'house not found')
     elif request.method == 'PUT':
         data = request.json
         if not {'latitude', 'longitude'} <= data.keys():
             return jsonify(create_response(400, 'bad request', 'latitude/longitude not found in body'))
-        return 'Hello World!'
+        for key, value in data.items():
+            try:
+                if key != 'family_count':
+                    data[key] = float(value)
+                else:
+                    data[key] = int(value)
+            except ValueError:
+                return create_response(400, 'bad request', f'invalid type for {key}')
+        if house:
+            house.latitude = data['latitude']
+            house.longitude = data['longitude']
+            house.family_count = data.get('family_count', house.family_count)
+
+        else:
+            house = House(house_id=house_id, latitude=data['latitude'], longitude=data['longitude'],
+                          family_count=data.get('family_count', 0))
+            db.session.add(house)
+        try:
+            db.session.commit()
+        except Exception as e:
+            return create_response(500, 'internal server error', 'there are some problem with database')
+        return jsonify(house.as_dict())
--- house/services/house_service.py
+++ house/services/house_service.py
@@ -1,6 +1,5 @@
-def hello():
-    return 'hello world'
+from flask import jsonify
 
 
 def create_response(code, name, description):
-    return dict(code=code, name=name, description=description)
+    return jsonify(dict(code=code, name=name, description=description))
